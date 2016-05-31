5a117cd45c010d243bda93d4238cdeb2ee93cd16
RomainRui <romain.rui10@gmail.com>
2016-05-31 16:18:57 +0200

    Initial Commit

---
+++ spitchy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  spitchy
+//
+//  Created by Rplay on 31/05/16.
+//  Copyright © 2016 rplay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
